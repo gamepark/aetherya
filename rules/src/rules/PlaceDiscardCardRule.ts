38161f340bbb5693ff3c8b043261400415923935
Romain Fromi <romain.fromi@gmail.com>
2024-05-15 20:33:12 +0200

    Simplify PlaceDiscardCardRule.getPlayerMoves

--- rules/src/rules/PlaceDiscardCardRule.ts
+++ rules/src/rules/PlaceDiscardCardRule.ts
@@ -7,35 +7,30 @@ import { RuleId } from './RuleId'
 
 export class PlaceDiscardCardRule extends AcquireLegendRule {
   getPlayerMoves(): MaterialMove[] {
-    // Card from the discard
-    const discardCardActions: MaterialMove[] = []
-    const discardCards = this.discardDeckCards()
-      .maxBy(item => item.location.x!)
-    const discardCardItems = discardCards.getItems()
-    if (discardCardItems.length == 0) return []
-    let discardCard = discardCardItems[0]
-    let discardCardId = discardCard.id
+    const moves: MaterialMove[] = super.getPlayerMoves()
 
-    this.material(MaterialType.KingdomCard)
+    const discardCard = this.discardDeckCards()
+      .maxBy(item => item.location.x!)
+    if (discardCard.length === 0) return []
+    const discardCardId = discardCard.getItem()!.id
+
+    const replaceableCards = this.material(MaterialType.KingdomCard)
       .location(LocationType.PlayerBoard)
       .player(this.player)
       .filter(item =>
         item.id === undefined || !item.location.rotation ||
-        (item.id != KingdomCard.Portal && item.id != KingdomCard.Dragon && item.id != discardCardId)
+        (item.id !== KingdomCard.Portal && item.id !== KingdomCard.Dragon && item.id !== discardCardId)
       )
       .getItems()
-      .forEach(item => {
-        discardCardActions.push(...discardCards.moveItems(
-          {
-            type: LocationType.PlayerBoard,
-            player: this.player, x: item.location.x, y: item.location.y, rotation: true
-          }))
-      })
 
-    return [
-      ...discardCardActions,
-      ...super.getPlayerMoves()
-    ]
+    for (const card of replaceableCards) {
+      moves.push(discardCard.moveItem({
+        type: LocationType.PlayerBoard,
+        player: this.player, x: card.location.x, y: card.location.y, rotation: true
+      }))
+    }
+
+    return moves
   }
 
   beforeItemMove(move: ItemMove): MaterialMove[] {

b01f2871f208351934cefe05e9ae7191e8671f7b
Romain Fromi <romain.fromi@gmail.com>
2024-06-05 15:37:50 +0200

    Shorten header texts

--- app/src/headers/ChooseCardHeader.tsx
+++ app/src/headers/ChooseCardHeader.tsx
@@ -1,26 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import { AetheryaRules } from '@gamepark/aetherya/AetheryaRules'
-import { AcquireLegendRule } from '@gamepark/aetherya/rules/AcquireLegendRule'
 import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { useTranslation } from 'react-i18next'
 
 export const ChooseCardHeader = () => {
   const { t } = useTranslation()
   const playerId = usePlayerId()
-  const rules=useRules<AetheryaRules>()
+  const rules = useRules<AetheryaRules>()
   const activePlayer = rules?.game.rule?.player
   const player = usePlayerName(activePlayer)
 
-  const currentRule:AcquireLegendRule=rules?.rulesStep as AcquireLegendRule
-  const availableLegendCards=currentRule.getPlayerLegendMoves().length>0
-
   if (playerId !== undefined && activePlayer === playerId) {
-    if (availableLegendCards)
-      return <>{t('header.choose-card-withLegend.you')}</>
-    return <>{t('header.choose-card-withoutLegend.you')}</>
+    return <>{t('header.choose-card.you')}</>
   } else {
-    if (availableLegendCards)
-      return <>{t('header.choose-card-withLegend.player', { player })}</>
-    return <>{t('header.choose-card-withoutLegend.player', { player })}</>
+    return <>{t('header.choose-card.player', { player })}</>
   }
 }
--- app/src/headers/PlaceDiscardHeader.tsx
+++ app/src/headers/PlaceDiscardHeader.tsx
@@ -1,26 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import { AetheryaRules } from '@gamepark/aetherya/AetheryaRules'
-import { AcquireLegendRule } from '@gamepark/aetherya/rules/AcquireLegendRule'
 import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { useTranslation } from 'react-i18next'
 
 export const PlaceDiscardHeader = () => {
   const { t } = useTranslation()
   const playerId = usePlayerId()
-  const rules=useRules<AetheryaRules>()
+  const rules = useRules<AetheryaRules>()
   const activePlayer = rules?.game.rule?.player
   const player = usePlayerName(activePlayer)
 
-  const currentRule:AcquireLegendRule=rules?.rulesStep as AcquireLegendRule
-  const availableLegendCards=currentRule.getPlayerLegendMoves().length>0
-
   if (playerId !== undefined && activePlayer === playerId) {
-    if (availableLegendCards)
-      return <>{t('header.place-discard-withLegend.you')}</>
-    return <>{t('header.place-discard-withoutLegend.you')}</>
+    return <>{t('header.place-discard.you')}</>
   } else {
-    if (availableLegendCards)
-      return <>{t('header.place-discard-withLegend.player', { player })}</>
-    return <>{t('header.place-discard-withoutLegend.player', { player })}</>
+    return <>{t('header.place-discard.player', { player })}</>
   }
 }
